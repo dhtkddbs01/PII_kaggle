5b3b6caf12a07dbfa304a91167c5ce5736d315ae
dhtkddbs <dhtkddbs2@gmail.com>
2024-02-02 01:36:39 +0900

    오타 수정

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 import json
 import pandas as pd
-impo
+import os
 SYS_INPUT_DIR = '/kaggle/input/pii-detection-removal-from-educational-data'
 train_json = json.load(open(os.path.join(SYS_INPUT_DIR, "train.json")))
 # json 파일 데이터 프레임으로 변환하기.
