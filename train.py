e426eee915bf6591b5e49fd001b9127b0787df60
dhtkddbs <dhtkddbs2@gmail.com>
2024-02-02 01:34:15 +0900

    라벨링 데이터 리스트추출

--- train.py
+++ train.py
@@ -1,11 +1,20 @@
 import json
 import pandas as pd
-import os
-
-# os를 통해 kaggle train data 불러오기
+impo
 SYS_INPUT_DIR = '/kaggle/input/pii-detection-removal-from-educational-data'
 train_json = json.load(open(os.path.join(SYS_INPUT_DIR, "train.json")))
 # json 파일 데이터 프레임으로 변환하기.
 train = pd.json_normalize(train_json)
+train.head()
 
-git
+# 라벨링 데이터가 무엇인지 찾기
+labels_name = []
+for i in train['labels']:
+    for r in i:
+        if r != 'O':
+            labels_name.append(r)
+# set으로 변환하여 중복 제거            
+labels_name = set(labels_name)
+# set으로 변환한 자료 다시 리스트로 변환
+labels_name_list = list(set(labels_name))
+# ['B-NAME_STUDENT', 'I-STREET_ADDRESS', 'B-EMAIL', 'B-URL_PERSONAL', 'I-NAME_STUDENT', 'B-PHONE_NUM', 'I-URL_PERSONAL', 'I-PHONE_NUM', 'B-USERNAME', 'B-STREET_ADDRESS', 'I-ID_NUM', 'B-ID_NUM'] 
